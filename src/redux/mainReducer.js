ab9e1a7efa103507479eca33c815781656bec611
Crowdbotics <team@crowdbotics.com>
2020-09-21 04:34:04 +0000

    add package blueprint-react-native-sign-in-4-screen

--- src/redux/mainReducer.js
+++ src/redux/mainReducer.js
@@ -6,6 +6,7 @@ import { combineReducers } from "redux";
 
 
 //@BlueprintReduxImportInsertion
+import SignIn45104494Reducer from '../features/SignIn45104494/redux/reducers'
 import SignIn56104493Reducer from '../features/SignIn56104493/redux/reducers'
 
 export const combinedReducers = combineReducers({
@@ -16,6 +17,7 @@ export const combinedReducers = combineReducers({
 
 
   //@BlueprintReduxCombineInsertion
+SignIn45104494: SignIn45104494Reducer,
 SignIn56104493: SignIn56104493Reducer,
 
 });
